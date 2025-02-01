c246b78836cf02c5b7cddfd91df9a73a8b27221d
Ararita-art <araratochtli@gmail.com>
2025-01-31 21:39:44 -0600

    Actualiza el archivo app.js

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 //Declarar primera variable array
-let listanombreAmigo = []
+let listaNombreAmigo = []
+
+//Desarrollar función ingresar nombreAmigo
+function ingresarNombreAmigo() {
+    const inputAmigo = document.getElementById("amigo");
+    const nombreAmigo = inputAmigo.value.trim();
+//Validar nombreAmigo
+    if (nombreAmigo === "") {
+        alert("Por favor, ingrese un nombre válido.");
+        return;
+    }
+//Añadir valores a listaNombreAmigo
+    listaNombreAmigo.push(nombreAmigo);
+
+    actualizarNominaAmigo();
+
+//Limpiar inputAmigo
+    inputAmigo.value = "";
+    inputAmigo.focus();
+}
